4793afe3163ce4ded580b9d61fbc0e6ebb42f811
mads94i5 <mads94i5@stud.kea.dk>
2024-01-12 16:56:32 +0100

    atlas

--- server/scripts/mongodb.js
+++ server/scripts/mongodb.js
@@ -1,7 +1,14 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const url = "mongodb://localhost:27017";
-const client = new MongoClient(url);
+const url = process.env.NODE_ENV === 'production' ? `mongodb+srv://mongouser:${process.env.MONGO_PASSWORD}@cluster0.dejan4e.mongodb.net/?retryWrites=true&w=majority` : "mongodb://localhost:27017";
+const client = new MongoClient(url, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
 await client.connect();
 
 const dbName = "universal_elements";
